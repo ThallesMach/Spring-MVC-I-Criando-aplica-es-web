69f53819b19916d74c2685e78ee7b2e9c6d78603
tls <tls@Machado-PC>
2018-10-27 01:52:12 -0300

    Update

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
+
 	@RequestMapping("/")
 	public String index() {
-		System.out.println("Entrando na Home Casa do Codigo!");
+		System.out.println("Entrando na home no casa de código");
 		return "home";
 	}
-
 }
