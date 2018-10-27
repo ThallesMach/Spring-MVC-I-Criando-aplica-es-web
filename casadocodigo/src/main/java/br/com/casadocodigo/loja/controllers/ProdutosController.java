c43215e6b3313087609d5459cfd053fd81668e63
tls <tls@Machado-PC>
2018-10-27 16:07:24 -0300

    _upDate SpringMVC-1 Aula_05 completa.

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
@@ -29,11 +30,15 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping( method=RequestMethod.POST)
-	public String gravar(Produto produto){
+	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes){
 		System.out.println(produto);
 		produtoDao.gravar(produto);
-		return "/produtos/ok";
+		
+		redirectAttributes.addFlashAttribute("Sucesso", "Produto Cadastrado com Sucesso! ");
+		
+		return new ModelAndView("redirect:produtos");
 	}
+	
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView listar() {
